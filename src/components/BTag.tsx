f00560f280b92a55da60be27deb1f91453780a01
chakAs3 <chakir.qatab@rhinos-me.com>
2023-07-29 18:15:03 +0400

    refactor : css

--- src/components/BTag.tsx
+++ src/components/BTag.tsx
@@ -2,11 +2,13 @@
 import { defineComponent } from 'vue';
 import MyButton from "./Button.vue"
 
+import './button.css'
+
 export const BTag = defineComponent({
 
      setup(props ,{ attrs, slots}){
 
-        return ()=> <div> BTAG <MyButton {...attrs }  /> </div> 
+        return ()=> <div> BTag : <MyButton {...attrs }  /> </div> 
 
      },
      
